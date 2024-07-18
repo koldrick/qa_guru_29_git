caaa400619e4dd639c9850e62e87e6f6e83f332c
Simon Kulakov <starckn1@gmail.com>
2024-07-18 15:12:19 +0300

    added get message method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,12 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+             System.out.println("i = " + i);
         }
+
+    }
+
+    private static  String getMessage() {
+        return "Hello World!";
     }
 }
